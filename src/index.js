0fb6cf9dd05a1f7371aa0aeeec006e463b1c772f
Anton <anton@adc.sh>
2018-06-11 07:56:13 +0100

    jsdoc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import { resolve } from 'path'
 import { readJSON, writeJSON, exists } from 'wrote'
 import { homedir as home } from 'os'
-import { askQuestions } from 'reloquent'
+import ask from 'reloquent'
+
+/**
+ * @attach reloquent
+ * @typedef {Object} Question
+ * @property {string} text A text to show to the user.
+ * @property {string} [defaultValue] A default answer to the question.
+ * @property {function} [getDefault] A function which will get the default value, possibly asynchronously.
+ * @property {function} [validation] A validation function which should throw on error.
+ * @property {(s: string) => string} [postProcess] A transformation function for the answer.
+ *
+ * @typedef {Object.<string, Question>} Questions
+ *
+ * @typedef {Object} AfricaConfig
+ * @property {boolean} [force=false] Force asking questions and re-writing config. Default false.
+ * @property {string} [homedir] Path to the home directory.
+ * @property {string} [defaultValue] A default answer to the question.
+ * @property {number} [questionsTimeout] How log to wait before timing out. Will wait forever by default.
+ * @property {(s: string) => string} [rcNameFunction] Function used to generate the rc name, e.g., packageName => `.${packageName}rc`,
+ */
+
 
 async function askQuestionsAndWrite(questions, path) {
-  const answers = await askQuestions(questions)
+  const answers = await ask(questions)
   await writeJSON(path, answers, { space: 2 })
   return answers
 }
@@ -13,20 +33,21 @@ async function askQuestionsAndWrite(questions, path) {
  * Read package configuration from the home directory, or ask questions with
  * readline interface to create a new configuration in `~/.${packageName}rc`
  * @param {string} packageName the name of the package
- * @param {object} questions an object with questions to be passed to reloquent
- * @param {object} config configuration object
- * @param {string} [config.force] Force asking questions and re-writing config
- * @param {string} [config.homedir] path to the home directory
- * @param {string} [config.questionsTimeout] how log to wait before timing out
- * @param {string} [config.rcNameFunction] Function used to generate the rc name
+ * @param {Questions} questions an object with questions to be passed to reloquent
+ * @param {AfricaConfig} [config] configuration object
+ * @param {boolean} [config.force=false] Force asking questions and re-writing config. Default false.
+ * @param {string} [config.homedir] Path to the home directory.
+ * @param {string} [config.defaultValue] A default answer to the question.
+ * @param {string} [config.questionsTimeout] How log to wait before timing out. Will wait forever by default.
+ * @param {(s: string) => string} [config.rcNameFunction] Function used to generate the rc name
  */
-async function africa(packageName, questions = {}, config = {}) {
+export default async function africa(packageName, questions = {}, config = {}) {
   if (typeof packageName != 'string') {
     throw new Error('Package name is required.')
   }
   const {
     homedir = home(),
-    rcNameFunction = packageName => `.${packageName}rc`,
+    rcNameFunction = p => `.${p}rc`,
     force = false,
   } = config
 
@@ -41,5 +62,3 @@ async function africa(packageName, questions = {}, config = {}) {
   const parsed = await readJSON(path)
   return parsed
 }
-
-module.exports = africa
